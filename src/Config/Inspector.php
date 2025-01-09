2791e188c0097a398f389fce858c9b09501c5ed0
Valerio Barbera <ilvalerione@gmail.com>
2025-01-09 14:36:02 +0100

    package refactoring

--- src/Config/Inspector.php
+++ src/Config/Inspector.php
@@ -7,16 +7,25 @@ use CodeIgniter\Config\BaseConfig;
 class Inspector extends BaseConfig
 {
     /**
-     * application ingestion key, you can find this on your inspector dashboard
+     * Application ingestion key, you can find this on your inspector dashboard
      *
      * @var string
      */
     public $ingestionKey = '';
 
     /**
+     * Master switch.
+     *
      * @var bool
      */
     public $enable = true;
+
+    /**
+     * Current version of the package.
+     *
+     * @var string
+     */
+    public $version = '0.5.0';
 
     /**
      * Remote endpoint to send data.
@@ -26,14 +35,15 @@ class Inspector extends BaseConfig
     public $url = 'https://ingest.inspector.dev';
 
     /**
-     * set this option to true if you want your application to send long running queries
-     * and query errors to the inspector dashboard. Default is false for backward compatibility.
+     * Determine if Inspector should monitor database interactions.
      *
      * @var bool
      */
     public $DBQuery = false;
 
     /**
+     * The strategy to send data.
+     *
      * @var string
      */
     public $transport = 'async';
