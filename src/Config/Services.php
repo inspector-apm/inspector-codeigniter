43e19ca7511f3933a2e2d7b23b79e7f41b9cad24
Valerio Barbera <ilvalerione@gmail.com>
2025-01-31 11:51:22 +0100

    custom exception handler

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -11,7 +11,7 @@ class Services extends BaseService
     /**
      * Current version of the package.
      */
-    public const VERSION = '0.6.1';
+    public const VERSION = '0.7.0';
 
     /**
      * Returns the Inspector manager class.
