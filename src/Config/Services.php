f018770baca6423313ab59458d3767be02a02035
Valerio Barbera <ilvalerione@gmail.com>
2025-03-07 12:14:37 +0100

    remove neuron ai component

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -11,7 +11,7 @@ class Services extends BaseService
     /**
      * Current version of the package.
      */
-    public const VERSION = '0.8.0';
+    public const VERSION = '0.9.0';
 
     /**
      * Returns the Inspector manager class.
